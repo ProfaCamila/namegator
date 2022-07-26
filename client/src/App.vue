ecf3fa54501092fbcc8d3c2f94998cb141f03d51
Camila <camila@email.com>
2022-07-26 19:46:48 -0300

    Commit Inicial
    Projeto Front-End

---
+++ client/src/App.vue
@@ -0,0 +1,39 @@
+<template>
+    <div>
+        <div id="slogan" class="text-center">
+            <h1>NameGator <span class="fa fa-check"></span></h1>
+            <br />
+            <h6 class="text-secondary">Gerador de nomes utilizando Vue.JS, GraphQL, Node</h6>
+        </div>
+        <div id="main">
+            <AppDomainList></AppDomainList>
+        </div>
+    </div>
+</template>
+
+<script>
+
+import "bootstrap/dist/css/bootstrap.css";
+import "font-awesome/css/font-awesome.css";
+import AppDomainList from "./components/AppDomainList.vue";
+
+export default {
+	name: "App",
+	components: {
+		AppDomainList,
+	}
+};
+</script>
+
+<style>
+#slogan {
+    margin-top: 30px;
+    margin-bottom: 30px;
+}
+
+#main {
+    background-color: darkgrey;
+    padding-top: 30px;
+    padding-bottom: 30px;
+}
+</style>
---
+++ client/src/components/AppItemList.vue
@@ -0,0 +1,56 @@
+<template>
+    <div>
+        <h5>Item: <span class="badge badge-info">{{ items.lenght }}</span></h5>
+        <div class="card">
+            <div class="card-body">
+                <ul class="list-group">
+                    <li class="list-group-item" v-for="item in items" v-bind:key="item">
+                        <div class="row">
+                            <div class="col-md">
+                                {{ item }}
+                            </div>
+                            <div class="col-md text-right">
+                                <button class="btn btn-info" v-on:click="removeItem(item)"><span
+                                        class="fa fa-trash"></span></button>
+                            </div>
+                        </div>
+                    </li>
+                </ul>
+                <br />
+                <div class="input-group">
+                    <input class="form-control" type="text" v-on:keyup.enter="addItem(item)" v-model="item"
+                        placeholder="Digite o item" />
+                    <div class="input-group-append">
+                        <button class="btn btn-info" v-on:click="addItem(item)"><span
+                                class="fa fa-plus"></span></button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+	name: "AppItemList",
+	props: ["items"],
+	data(){
+		return {
+			item: ""
+		};
+	},
+	methods: {
+		addItem(item) {
+			this.$emit("addItem", item);
+			this.item = "";			
+		},
+		removeItem(item) {
+			this.$emit("deleteItem", item);
+		}
+	}
+};
+</script>
+
+
+<style scoped>
+</style>
